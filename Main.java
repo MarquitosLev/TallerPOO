d20b4ffd72e721703681b5c8e98a281c1d11c61d
gosti8 <112736214+gosti8@users.noreply.github.com>
2022-09-02 21:56:21 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-  public static void main(String[] args){
-    System.out.println("Hola Mundo"); 
-    System.out.println(2 +2);
-    System.out.println("tukson");
-    System.out.println("anachei");
-  } 
-}
