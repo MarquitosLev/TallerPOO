d73a40422a4160a1657d0f9b125f996f64b61048
Marcos Leiva <marcleiva623@gmail.com>
2022-09-15 00:39:34 -0300

    Creacion clase obra, falta agregar las enumeraciones

---
+++ src/main/java/Obra.java
@@ -0,0 +1,80 @@
+
+public class Obra {
+	//private tipoObra tipo;
+	private int cantEjem;
+	private int cantEjemDisponible;
+	private String titulo;
+	private String subtitulo;
+	private String autor1;
+	private String autor2;
+	private String autor3;
+	private String genero;
+	private int isbn;
+	private int id;
+	//private area area;
+	
+	// Faltan set y get de tipoObra y area
+	
+	public int getCantEjem() {
+		return cantEjem;
+	}
+	public void setCantEjem(int cantEjem) {
+		this.cantEjem = cantEjem;
+	}
+	public int getCantEjemDisponible() {
+		return cantEjemDisponible;
+	}
+	public void setCantEjemDisponible(int cantEjemDisponible) {
+		this.cantEjemDisponible = cantEjemDisponible;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getSubtitulo() {
+		return subtitulo;
+	}
+	public void setSubtitulo(String subtitulo) {
+		this.subtitulo = subtitulo;
+	}
+	public String getAutor1() {
+		return autor1;
+	}
+	public void setAutor1(String autor1) {
+		this.autor1 = autor1;
+	}
+	public String getAutor2() {
+		return autor2;
+	}
+	public void setAutor2(String autor2) {
+		this.autor2 = autor2;
+	}
+	public String getAutor3() {
+		return autor3;
+	}
+	public void setAutor3(String autor3) {
+		this.autor3 = autor3;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public int getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+}
