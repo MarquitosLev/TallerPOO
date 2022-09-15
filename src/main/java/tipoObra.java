cd0766e7ac3c93db8bb179648e2a6e6f43218cec
Santiago Fernandez Gomez <santifzgz@gmail.com>
2022-09-15 17:21:11 -0300

    Creacion enumeracion tipoObra

---
+++ src/main/java/tipoObra.java
@@ -0,0 +1,3 @@
+public enum tipoObra {
+    libro, revista, ensayo, tesis, manual, boletin, coleccion, compendio, diario, diccionario, documental, enciclopedia, investigacion, monografia, publicacionMensual, otro;
+}
