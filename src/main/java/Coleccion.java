0f008678608a80a4b1b28e32b7d647eb679c8045
Marcos Leiva <marcleiva623@gmail.com>
2022-09-15 00:46:28 -0300

    Creacion clase Coleccion

---
+++ src/main/java/Coleccion.java
@@ -0,0 +1,14 @@
+
+public class Coleccion {
+	private String nomColeccion;
+
+	public String getNomColeccion() {
+		return nomColeccion;
+	}
+
+	public void setNomColeccion(String nomColeccion) {
+		this.nomColeccion = nomColeccion;
+	}
+	
+	
+}
