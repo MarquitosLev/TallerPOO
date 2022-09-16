c56b9e98b7ee5c98674b88e8d298db468542f497
Santiago Fernandez Gomez <santifzgz@gmail.com>
2022-09-16 14:45:36 -0300

    Creacion clase Funcionario

---
+++ src/main/java/Funcionario.java
@@ -0,0 +1,25 @@
+public class Funcionario {
+	private String usuario;
+	private String contrasenia;
+	
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getContrasenia() {
+		return contrasenia;
+	}
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+	
+	public void registrarObservaciones() {
+		//falta completar
+	}
+	
+	public void realizarPrestamo() {
+		//falta completar
+	}
+}
