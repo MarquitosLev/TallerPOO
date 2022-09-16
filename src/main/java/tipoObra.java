85df4aa49761ed204c6374e77860cd4670c47e31
Santiago Fernandez Gomez <santifzgz@gmail.com>
2022-09-16 13:29:16 -0300

    Enum tipoObra

--- src/main/java/tipoObra.java
+++ src/main/java/tipoObra.java
@@ -1,3 +1,3 @@
 public enum tipoObra {
-    Libro, Revista, Ensayo, Tesis, Manual, Boletin, Coleccion, Compendio, Diario, Diccionario, Documental, Enciclopedia, Investigacion, Monografia, PublicacionMensual, Otro;
+	
 }
