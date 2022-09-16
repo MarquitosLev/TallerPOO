93ed942c9bfceebc9b9be31db4c926edf3109dfd
Santiago Fernandez Gomez <santifzgz@gmail.com>
2022-09-16 14:21:31 -0300

    Creación clase Multa

---
+++ src/main/java/Multa.java
@@ -0,0 +1,11 @@
+public class Multa {
+	private int diasMulta;
+	
+	public int getDiasMulta() {
+		return diasMulta;
+	}
+	
+	public void setDiasMulta(int diasMulta) {
+		this.diasMulta = diasMulta;
+	}
+}
