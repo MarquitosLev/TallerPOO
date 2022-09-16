d0a5f8d526194e94020ab4f14cf04ed2af28fa1c
gosti8 <sebastiandanielgosti8@gmail.com>
2022-09-16 14:28:02 -0300

    Creación Clases Persona - Lector - Préstamo
    Falta constructor default para Prestamo

---
+++ src/main/java/Prestamo.java
@@ -0,0 +1,87 @@
+import java.util.Date;
+
+public class Prestamo {
+	private Date fechaHoraPrestada;
+	private String funcionarioPrestador;
+	private Date fechaHoraADevolver;
+	private Date fechaDevuelta;
+	private String funcionarioDevuelta;
+	private Lector lector;
+	private Boolean aDomicilio;
+	
+	public Prestamo(Date fechaHoraPrestada, String funcionarioPrestador, Date fechaHoraADevolver, Date fechaDevuelta,
+			String funcionarioDevuelta, Lector lector, Boolean aDomicilio) {
+		this.fechaHoraPrestada = fechaHoraPrestada;
+		this.funcionarioPrestador = funcionarioPrestador;
+		this.fechaHoraADevolver = fechaHoraADevolver;
+		this.fechaDevuelta = fechaDevuelta;
+		this.funcionarioDevuelta = funcionarioDevuelta;
+		this.lector = lector;
+		this.aDomicilio = aDomicilio;
+	}
+
+	public Prestamo() {
+
+
+	}
+
+	public Date getFechaHoraPrestada() {
+		return fechaHoraPrestada;
+	}
+
+	public void setFechaHoraPrestada(Date fechaHoraPrestada) {
+		this.fechaHoraPrestada = fechaHoraPrestada;
+	}
+
+	public String getFuncionarioPrestador() {
+		return funcionarioPrestador;
+	}
+
+	public void setFuncionarioPrestador(String funcionarioPrestador) {
+		this.funcionarioPrestador = funcionarioPrestador;
+	}
+
+	public Date getFechaHoraADevolver() {
+		return fechaHoraADevolver;
+	}
+
+	public void setFechaHoraADevolver(Date fechaHoraADevolver) {
+		this.fechaHoraADevolver = fechaHoraADevolver;
+	}
+
+	public Date getFechaDevuelta() {
+		return fechaDevuelta;
+	}
+
+	public void setFechaDevuelta(Date fechaDevuelta) {
+		this.fechaDevuelta = fechaDevuelta;
+	}
+
+	public String getFuncionarioDevuelta() {
+		return funcionarioDevuelta;
+	}
+
+	public void setFuncionarioDevuelta(String funcionarioDevuelta) {
+		this.funcionarioDevuelta = funcionarioDevuelta;
+	}
+
+	public Lector getLector() {
+		return lector;
+	}
+
+	public void setLector(Lector lector) {
+		this.lector = lector;
+	}
+
+	public Boolean getaDomicilio() {
+		return aDomicilio;
+	}
+
+	public void setaDomicilio(Boolean aDomicilio) {
+		this.aDomicilio = aDomicilio;
+	}
+	
+	public void compararFecha() {
+		
+	}
+}
